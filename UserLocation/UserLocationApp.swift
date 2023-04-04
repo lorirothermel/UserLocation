70c04f8b54ecfa9cb3803fdc3d011630e1c6f344
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-04 04:31:51 -0400

    Initial Commit

---
+++ UserLocation/UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationApp.swift
+//  UserLocation
+//
+//  Created by Lori Rothermel on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
